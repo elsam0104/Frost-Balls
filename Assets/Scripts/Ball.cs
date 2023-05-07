46c74ef4983c127109396f2da7b8144aeeb88c70
YeEun Oh <OhSolDev@gmail.com>
2023-05-08 00:38:39 +0900

    공 추가

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private void Start()
+    {
+        FindObjectOfType<DeformPlane>().DeformMesh(transform.position);
+    }
+}
